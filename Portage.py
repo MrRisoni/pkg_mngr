76768a0eafc8f01c487eec530f9b495b41162980
MrRisoni <vanguelisk@gmail.com>
2020-02-23 19:22:41 +0200

    first folders

---
+++ Portage.py
@@ -0,0 +1,7 @@
+class Portage:
+
+    def __init__(self):
+        pass
+
+    def search(self, pkg_name):
+        print 'Searching for ' + pkg_name
---
+++ emerge.py
@@ -0,0 +1,17 @@
+import sys
+from Portage import Portage
+options = ['s']
+
+
+p =  Portage()
+
+
+if len(sys.argv) ==1:
+    print 'No arguments.Valid arguments are'
+    print options
+else:
+    #args_list = sys.argv.split(' ')
+    #print args_list
+    args_list = sys.argv[1:]
+    print args_list
+    p.search(args_list[1])
