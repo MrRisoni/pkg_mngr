e307df662e83af4fc05ffb5285183372e8401b87
MrRisoni <vanguelisk@gmail.com>
2020-03-04 09:09:43 +0200

    read installed

--- Portage.py
+++ Portage.py
@@ -8,11 +8,25 @@ class Portage:
     def __init__(self):
         self.REPO_PATH = 'repository'
         self.PKG_ENDING = '.json'
-        pass
+
+        self.installed = [x.strip() for x in open('installed', 'r').readlines()]
+
+        print (self.installed)
+
+
+    def installPackage(self,pkgItm):
+        print ('Needs pkg ' + pkgItm.name + ' ' + pkgItm.operate + ' ' + pkgItm.pattern)
+
+
+    def installOrder(self,packages):
+        for pkg in packages:
+            for dep in pkg.depends:
+                self.installPackage(dep)
+
 
 
     def search(self, search_term):
-        print 'Searching for ' + search_term
+        print ('Searching for ' + search_term)
         #print self.REPO_PATH
         result = []
         for root, dirs, packages in os.walk(self.REPO_PATH):
@@ -22,10 +36,10 @@ class Portage:
                 if search_term in each_package:
                     #result.append(os.path.join(root, search_term))
                     pgk_build_file = os.path.join(root, each_package)
-                    print pgk_build_file
+                    #print pgk_build_file
 
                     pkg_contents = {}
                     with open(pgk_build_file, 'r') as file:
                         json_str = file.read().replace('\n', '')
                         pkg_contents = json.loads(json_str)
-        print pkg_contents
+        print (pkg_contents)
--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import json
+import yaml
+from collections import namedtuple
+
 from PackageDescr.Package import Package
 from PackageDescr.Dependency import Dependency
 from PackageDescr.ChoiceDependency import ChoiceDependency
 
 from PackageDescr.Choices import Choices
 
+from Portage import Portage
 
 
 dp = Dependency("base-system/glibc", "gte", "4.6.*")
@@ -19,5 +23,15 @@ gui = Choices("gui",[qt, gtk], False)
 pkg = Package("apps-media/rosegarden","I am","2020021822T34Z","5..6.7",[],[dp,fftw],[],gui)
 
 
-json_data = json.dumps( pkg, default=lambda o: o.__dict__, indent=4,sort_keys=False)
-print(json_data)
+with open('repository/apps-media/rosegarden.yml') as f:
+    data = yaml.load(f)
+    #print(data)
+
+
+#json_data = json.dumps( pkg, default=lambda o: o.__dict__, indent=4,sort_keys=False)
+#\print(json_data)
+
+
+
+emerge = Portage()
+emerge.installOrder([pkg])
