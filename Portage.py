b9b8dea5ab144a27a98e59393199636735654ddc
MrRisoni <vanguelisk@gmail.com>
2020-03-04 09:42:18 +0200

    version parsing

--- Portage.py
+++ Portage.py
@@ -9,14 +9,46 @@ class Portage:
         self.REPO_PATH = 'repository'
         self.PKG_ENDING = '.json'
 
-        self.installed = [x.strip() for x in open('installed', 'r').readlines()]
-
+        raw = [x.strip() for x in open('installed', 'r').readlines()]
+        self.installed = {}
+        for r in raw:
+            details = r.split('::');
+            #print (details)
+            self.installed[details[0]] = [details[1]]
         print (self.installed)
+
+    def versionCompare(self,installed,operator,needed):
+        # unit test this
+        #print ('version compare')
+        #print (installed,operator,needed)
+        version_rules = needed.split('.')
+        major_needed = version_rules[0]
+        minor_needed = version_rules[1]
+        revision_needed = version_rules[2]
+
+        installed_rules = installed[0].split('.')
+        version_rules_installed = installed_rules
+        major_installed = version_rules_installed[0]
+        minor_installed = version_rules_installed[1]
+        revision_installed = version_rules_installed[2]
+
+        print (version_rules_installed)
+        print (version_rules)
+        needs_update = False
+
+        if major_needed > major_installed:
+            needs_update = true
+        if major_needed >=  major_installed &&    
+
 
 
     def installPackage(self,pkgItm):
-        print ('Needs pkg ' + pkgItm.name + ' ' + pkgItm.operate + ' ' + pkgItm.pattern)
-
+        details = pkgItm.name.split('/')
+        if details[1] not in self.installed:
+            print ('Needs pkg ' + pkgItm.name + ' ' + pkgItm.operate + ' ' + pkgItm.pattern)
+        else:
+            # check version
+            self.versionCompare(self.installed[details[1]],pkgItm.operate,pkgItm.pattern)
 
     def installOrder(self,packages):
         for pkg in packages:
